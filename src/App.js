aeb09dca3ed8bb78ef2e0c357914af5b8bbcde34
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-28 23:19:43 -0400

    Updated project to used react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
-            )}
-          />
-          <Redirect to="/" />
-        </Switch>
+        <Outlet />
       </div>
-    </Router>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { worker } from './api/server'
 
@@ -15,7 +16,20 @@ async function start() {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route
+                index
+                element={
+                  <section>
+                    <h2>Welcome to the Redux Essentials example app!</h2>
+                  </section>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </React.StrictMode>,
     document.getElementById('root')
