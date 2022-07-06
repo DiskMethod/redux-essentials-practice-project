00cc5d3aa529a9cfbc8d6617ee86a000d972e1b9
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-07-05 20:18:10 -0400

    RTK Query for PostsList and SinglePostPage

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postsSlice";
 import usersReducer from "../features/users/usersSlice";
 import userNotifications from "../features/notifications/notificationsSlice";
+import { apiSlice } from "../api/apiSlice";
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: userNotifications,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
+
 import { useParams, Link, Outlet } from "react-router-dom";
-import { selectPostById } from "./postsSlice";
+
+import Spinner from "../../components/Spinner";
+import { useGetPostQuery } from "../../api/apiSlice";
 
 import PostNotfound from "./PostNotFound";
 import PostAuthor from "./PostAuthor";
@@ -11,29 +13,35 @@ import ReactionButtons from "./ReactionButtons";
 const SinglePostPage = () => {
   const { postId } = useParams();
 
-  const post = useSelector((state) => selectPostById(state, postId));
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId);
 
-  if (!post) {
-    return <PostNotfound />;
+  let content;
+
+  if (isFetching) {
+    content = <Spinner text="Loading..." />;
+  } else if (isSuccess) {
+    content = (
+      <>
+        <article className="post">
+          <h2>{post.title}</h2>
+          <div>
+            <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
+          </div>
+          <p className="post-content">{post.content}</p>
+          <ReactionButtons post={post} />
+          <Link to="edit" className="button">
+            Edit Post
+          </Link>
+        </article>
+        <Outlet />
+      </>
+    );
+  } else {
+    content = <PostNotfound />;
   }
 
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content}</p>
-        <ReactionButtons post={post} />
-        <Link to="edit" className="button">
-          Edit Post
-        </Link>
-      </article>
-      <Outlet />
-    </section>
-  );
+  return <section>{content}</section>;
 };
 
 export default SinglePostPage;
